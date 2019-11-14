2dbf11fdb9564f77a6e2b03b86ecd49af2321b83
emersonpalacio <emersonpalaciootalvaro@gmail.com>
2019-11-14 00:54:47 -0500

    User

---
+++ MyVet.Web/Models/LoginViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyVet.Web.Models
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Username { get; set; }
+
+        [Required]
+        [MinLength(6)]
+        public string Password { get; set; }
+
+        //yo quiero qe el usuario guarde sección
+        public bool RememberMe { get; set; }
+
+    }
+}
